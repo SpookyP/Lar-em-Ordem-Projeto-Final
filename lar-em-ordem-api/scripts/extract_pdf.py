09a5479a7a59c55e2508077f5fe4b0a5fdd065bd
João Ribeiro <128801212+FKSHyper@users.noreply.github.com>
2026-09-23 15:30:44 +0100

    Comentários sobre o código

--- lar-em-ordem-api/scripts/extract_pdf.py
+++ lar-em-ordem-api/scripts/extract_pdf.py
@@ -1,17 +1,17 @@
 import sys
 import json
-import pdfplumber
+import pdfplumber """ Uma biblioteca especializada na extração de texto e dados de ficheiros PDF """
 
 def extract_data(pdf_path):
     try:
-        with pdfplumber.open(pdf_path) as pdf:
-            first_page = pdf.pages[0]
-            text = first_page.extract_text()
+        with pdfplumber.open(pdf_path) as pdf: """ Abre o PDF localizado no caminho (pdf_path) """
+            first_page = pdf.pages[0] """ Acede a primeira página do documento """
+            text = first_page.extract_text() """ Extrai todo o texto legível dessa primeira página """
 
             return json.dumps({
-                "status": "success",
-                "extracted_text": text[0:200],
-                "expiration_date": "2030-12-31"
+                "status": "success",  """ Estado """
+                "extracted_text": text[0:200], """ Os primeiros 200 caracteres do texto extraído """
+                "expiration_date": "2030-12-31" """ Uma data de validade simulada (placeholder) """
             })
     except Exception as e:
         return json.dumps({"status": "error", "message": str(e)})
